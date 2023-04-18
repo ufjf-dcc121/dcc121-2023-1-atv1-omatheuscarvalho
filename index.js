637bdb18d31ae4334c930d9a9cf11f79cd20282f
Matheus Carvalho <matheusc200@gmail.com>
2023-04-18 10:57:38 -0300

    Adicionado uma func pro botão de submit

---
+++ index.js
@@ -0,0 +1,6 @@
+const submitBtn = document.getElementById('submit-btn');
+document.addEventListener('click', (e) => {
+  if (e.target === submitBtn) {
+    alert("Obrigado pelo contato, responderemos em breve! :3")
+  }
+});
